031c3fa1acfee3a6274882f837014a8ecb7a5238
David Eckhard <dabombeeer@gmail.com>
2023-02-16 17:36:47 +0100

    global: move notifications from invenio-requests

--- invenio_notifications/config.py
+++ invenio_notifications/config.py
@@ -1,9 +1,89 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2023 CERN.
+# Copyright (C) 2023 Graz University of Technology.
 #
 # Invenio-Notifications is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
 """Invenio module for notifications support."""
+
+from flask_babelex import gettext as _
+from invenio_records_resources.services.base.config import ConfiguratorMixin, FromConfig
+
+from .backends.email.backend import EmailNotificationBackend
+from .events import (
+    CommunityInvitationAcceptedEvent,
+    CommunityInvitationCreatedEvent,
+    CommunityInvitationDeclinedEvent,
+    CommunitySubmissionCreatedEvent,
+    CommunitySubmissionDeclinedEvent,
+    CommunitySubmissionDeletedEvent,
+    CommunitySubmissionSubmittedEvent,
+)
+
+
+class NotificationConfig(ConfiguratorMixin):
+    """Config."""
+
+    backends = FromConfig("NOTIFICATIONS_BACKENDS", {})
+    notification_policy = FromConfig("NOTIFICATIONS_POLICY", {})
+
+
+# NOTIFICATIONS_CONFIG = NotificationConfig
+
+NOTIFICATIONS_BACKENDS = {
+    EmailNotificationBackend.id: EmailNotificationBackend,
+}
+
+NOTIFICATIONS_DEFAULT_SUBJECT = _("New notification from repository")
+
+NOTIFICATIONS_POLICY = {
+    CommunitySubmissionSubmittedEvent.handling_key: {
+        "backends": [
+            EmailNotificationBackend.id,
+        ],
+    },
+    CommunitySubmissionCreatedEvent.handling_key: {
+        "backends": [
+            EmailNotificationBackend.id,
+            # 'text',
+        ],
+    },
+    CommunitySubmissionDeclinedEvent.handling_key: {
+        "backends": [
+            EmailNotificationBackend.id,
+            # 'text',
+        ],
+    },
+    CommunitySubmissionDeletedEvent.handling_key: {
+        "backends": [
+            EmailNotificationBackend.id,
+            # 'text',
+        ],
+    },
+    "comment_created": {
+        "backends": [
+            EmailNotificationBackend.id,
+            # 'slack',
+        ],
+        # 'recipients': []
+    },
+    "invitation_expired": {"backends": []},
+    CommunityInvitationCreatedEvent.handling_key: {
+        "backends": [
+            EmailNotificationBackend.id,
+        ],
+    },
+    CommunityInvitationAcceptedEvent.handling_key: {
+        "backends": [
+            EmailNotificationBackend.id,
+        ],
+    },
+    CommunityInvitationDeclinedEvent.handling_key: {
+        "backends": [
+            EmailNotificationBackend.id,
+        ],
+    },
+}
--- invenio_notifications/ext.py
+++ invenio_notifications/ext.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2023 CERN.
+# Copyright (C) 2023 Graz University of Technology.
 #
 # Invenio-Notifications is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -9,6 +10,8 @@
 """Invenio module for notifications support."""
 
 from . import config
+from .config import NotificationConfig
+from .manager import NotificationManager
 
 
 class InvenioNotifications(object):
@@ -22,6 +25,7 @@ class InvenioNotifications(object):
     def init_app(self, app):
         """Flask application initialization."""
         self.init_config(app)
+        self.init_manager(app)
         app.extensions["invenio-notifications"] = self
 
     def init_config(self, app):
@@ -29,3 +33,15 @@ class InvenioNotifications(object):
         for k in dir(config):
             if k.startswith("NOTIFICATIONS_"):
                 app.config.setdefault(k, getattr(config, k))
+
+    def init_manager(self, app):
+        """Initialize manager."""
+        cfg = NotificationConfig.build(app)
+        manager = NotificationManager(
+            config=cfg,
+        )
+        for id, backend_cls in cfg.backends.items():
+            manager.register(backend_cls())
+
+        manager.validate_policy()
+        self.manager = manager
