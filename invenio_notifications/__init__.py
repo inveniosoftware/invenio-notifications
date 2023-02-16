a671aead36552ef49aec11d73d3b3d8b0c9b1975
David Eckhard <dabombeeer@gmail.com>
2023-02-16 17:36:47 +0100

    global: move notifications from invenio-requests

--- invenio_notifications/__init__.py
+++ invenio_notifications/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2023 CERN.
+# Copyright (C) 2023 Graz University of Technology.
 #
 # Invenio-Notifications is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -9,10 +10,13 @@
 """Invenio module for notifications support."""
 
 from .ext import InvenioNotifications
+from .proxies import current_notifications, current_notifications_manager
 
 __version__ = "0.1.0"
 
 __all__ = (
     "__version__",
+    "current_notifications",
+    "current_notifications_manager",
     "InvenioNotifications",
 )
