39733b7449c44629f639cd2178ce106d46e9b62d
David Eckhard <dabombeeer@gmail.com>
2023-09-13 15:28:37 +0200

    generators: add conditional recipient generator

--- invenio_notifications/services/filters.py
+++ invenio_notifications/services/filters.py
@@ -10,6 +10,8 @@
 
 from abc import ABC, abstractmethod
 
+from invenio_records.dictutils import dict_lookup
+
 
 class RecipientFilter(ABC):
     """Recipient filter for a notification."""
@@ -18,3 +20,23 @@ class RecipientFilter(ABC):
     def __call__(self, notification, recipients):
         """Filter recipients."""
         raise NotImplementedError()
+
+
+class KeyRecipientFilter(RecipientFilter):
+    """Recipient filter based on a given key."""
+
+    def __init__(self, key):
+        """Initialize with key for lookup."""
+        super().__init__()
+        self._key = key
+
+    def __call__(self, notification, recipients):
+        """Filter recipients."""
+        recipient_key = dict_lookup(notification.context, self._key)
+        if not isinstance(recipient_key, str):
+            return recipients
+
+        if recipient_key in recipients:
+            del recipients[recipient_key]
+
+        return recipients
