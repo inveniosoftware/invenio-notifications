19e84e31acd45d864b8fd5cc0a2bcb88dfa9787f
Sam Arbid <samaa2@kth.se>
2025-10-21 11:33:06 +0200

    📦 release: v1.2.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,7 @@
 ..
     Copyright (C) 2023 CERN.
     Copyright (C) 2024 Graz University of Technology.
+    Copyright (C) 2025 KTH Royal Institute of Technology.
 
     Invenio-Notifications is free software; you can redistribute it and/or
     modify it under the terms of the MIT License; see LICENSE file for more
@@ -8,6 +9,10 @@
 
 Changes
 =======
+
+Version v1.2.2 (released 2025-10-21)
+
+- i18n: pulled translations
 
 Version v1.2.1 (released 2025-08-29)
 
--- invenio_notifications/__init__.py
+++ invenio_notifications/__init__.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2023-2025 CERN.
 # Copyright (C) 2023-2024 Graz University of Technology.
+# Copyright (C) 2025 KTH Royal Institute of Technology.
 #
 # Invenio-Notifications is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -12,7 +13,7 @@
 from .ext import InvenioNotifications
 from .proxies import current_notifications, current_notifications_manager
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 __all__ = (
     "__version__",
