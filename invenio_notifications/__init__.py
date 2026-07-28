f3354c92278e3da81fa81c58e5dc13d62e7d31e8
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-07-28 10:09:49 +0200

    release: v1.4.0

--- invenio_notifications/__init__.py
+++ invenio_notifications/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2023-2025 CERN.
-# SPDX-FileCopyrightText: 2023-2024 Graz University of Technology.
+# SPDX-FileCopyrightText: 2023-2026 Graz University of Technology.
 # SPDX-FileCopyrightText: 2025 KTH Royal Institute of Technology.
 # SPDX-License-Identifier: MIT
 
@@ -8,7 +8,7 @@
 from .ext import InvenioNotifications
 from .proxies import current_notifications, current_notifications_manager
 
-__version__ = "1.3.1"
+__version__ = "1.4.0"
 
 __all__ = (
     "__version__",
